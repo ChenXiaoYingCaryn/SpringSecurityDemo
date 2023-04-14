f7a91545640904a9561eb11213dea92297b9ea58
ChenXiaoYingCaryn <739130811@qq.com>
2023-04-15 01:43:27 +0800

    add -- 项目使用nacos进行项目配置管理

--- src/main/java/com/example/springsecuritydemo3/Springsecuritydemo3Application.java
+++ src/main/java/com/example/springsecuritydemo3/Springsecuritydemo3Application.java
@@ -1,9 +1,11 @@
 package com.example.springsecuritydemo3;
 
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@NacosPropertySource(dataId = "SpringSecurityDemo.application.properties", autoRefreshed = true)
 public class Springsecuritydemo3Application {
 
     public static void main(String[] args) {
